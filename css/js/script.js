cafd4e71a4cb9c83ad07bbcc506bfc0ca73d5bdd
faris123 <119119760+oldie123@users.noreply.github.com>
2023-09-23 07:04:57 +0700

    update form

--- css/js/script.js
+++ css/js/script.js
@@ -1,19 +1,26 @@
 // navbar-fixed
-window.onscroll = function(){
-    const header = document.querySelector('header');
+window.onscroll = function () {
+    const header = document.querySelector("header");
     const fixednav = header.offsetTop;
 
-    if(window.pageYOffset > fixednav) {
-        header.classList.add('navbar-fixed');
+    if (window.pageYOffset > fixednav) {
+        header.classList.add("navbar-fixed");
     } else {
-        header.classList.remove('navbar-fixed');
+        header.classList.remove("navbar-fixed");
     }
 };
 
 // hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
 });
+
+// clear before unload
+window.onbeforeunload = () => {
+    for (const from of document.getElementsByTagName("form")) {
+        from.reset();
+    }
+};
